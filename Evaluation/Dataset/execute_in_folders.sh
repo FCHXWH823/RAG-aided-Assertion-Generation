9a629e1a18022d4c7b94a47fce374b536107ae9b
Weihua <fch@10-18-176-159.dynapool.wireless.nyu.edu>
2025-02-05 19:33:10 -0500

    Update Dataset

---
+++ Evaluation/Dataset/execute_in_folders.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Name of the executable file
+EXECUTABLE="fpv.tcl"
+
+# Loop through all directories in the current folder
+for dir in */; do
+    if [[ -d "$dir" && -x "$dir/$EXECUTABLE" ]]; then
+        echo "Executing $EXECUTABLE in $dir"
+        (cd "$dir" && jg -no_gui "$EXECUTABLE" && cd ..)
+    else
+        echo "Skipping $dir (executable not found or not executable)"
+    fi
+done
