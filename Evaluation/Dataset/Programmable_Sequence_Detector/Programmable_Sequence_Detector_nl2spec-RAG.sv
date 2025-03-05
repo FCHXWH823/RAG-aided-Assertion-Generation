e255a78a3577d222929eb3cc3a70186f4c8bccf9
Weihua <fch@10-18-176-159.dynapool.wireless.nyu.edu>
2025-03-05 11:18:26 -0500

    nl2spec 10 textbooks

---
+++ Evaluation/Dataset/Programmable_Sequence_Detector/Programmable_Sequence_Detector_nl2spec-RAG.sv
@@ -0,0 +1,35 @@
+module Programmable_Sequence_Detector 
+#(
+	parameter SEQ_W = 5
+)
+(
+  input clk,
+  input resetn,
+  input [SEQ_W-1:0] init,
+  input             din,
+  input logic      seen
+);
+
+reg  [SEQ_W-1:0] seq_q;
+wire [SEQ_W-1:0] seq_next;
+
+assign seq_next = { seq_q[SEQ_W-2:0] , din } ;
+
+always @(posedge clk) 
+begin
+	if ( ~resetn ) begin
+		seq_q <= '0;
+	end else begin
+		seq_q <= seq_next;
+	end
+end
+
+assign seen = seq_q == init;
+
+
+assert property(@(posedge clk) disable iff(~resetn) seen == (seq_q == init));
+
+assert property (@(posedge clk) disable iff(~resetn) (s_eventually (seen == 1)));
+assert property (@(posedge clk) disable iff(~resetn) (seen == (seq_q == init)) iff (s_eventually (seen == 1)));
+
+endmodule
