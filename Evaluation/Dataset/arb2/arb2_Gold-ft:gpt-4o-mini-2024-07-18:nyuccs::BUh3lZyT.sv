1dccc29ae908de20222143101509b34a46a1870b
Weihua <fch@10-18-180-35.dynapool.wireless.nyu.edu>
2025-05-23 21:18:34 -0400

    GoldCheck-Finetune-LowTemperature

---
+++ Evaluation/Dataset/arb2/arb2_Gold-ft:gpt-4o-mini-2024-07-18:nyuccs::BUh3lZyT.sv
@@ -0,0 +1,55 @@
+module arb2(clk, rst, req1, req2, gnt1, gnt2);
+
+input clk, rst;
+input req1, req2;
+output gnt1, gnt2;
+
+reg state;
+reg gnt1, gnt2;
+
+always @ (posedge clk or posedge rst)
+	if (rst)
+		state <= 0;
+	else
+		state <= gnt1;
+
+always @ (*)
+	if (state)
+	begin
+		gnt1 = req1 & ~req2;
+		gnt2 = req2;
+	end
+	else
+	begin
+		gnt1 = req1;
+		gnt2 = req2 & ~req1;
+	end
+
+
+assert property(@(posedge clk) (state == 1 & req2 == 1) |-> (gnt1 == 0));
+assert property(@(posedge clk) (req1 == 1 & state == 0) |-> (gnt1 == 1));
+assert property(@(posedge clk) (req1 == 0) |-> (gnt1 == 0));
+assert property(@(posedge clk) (req1 == 1 & req2 == 0) |-> (gnt1 == 1));
+assert property(@(posedge clk) (req1 == 1 & state == 0) |-> (gnt2 == 0));
+assert property(@(posedge clk) (req2 == 1 & state == 1) |-> (gnt2 == 1));
+assert property(@(posedge clk) (req2 == 0) |-> (gnt2 == 0));
+assert property(@(posedge clk) (req2 == 1 & req1 == 0) |-> (gnt2 == 1));
+
+assert property (@(posedge clk)  ((state == 1 && req2 == 1) |-> (gnt1 == 0)));
+assert property (@(posedge clk)  ((state == 1 & req2 == 1) |-> (gnt1 == 0)) iff ((state == 1 && req2 == 1) |-> (gnt1 == 0)));
+assert property (@(posedge clk)  ((req1 == 1 && state == 0) |-> (gnt1 == 1)));
+assert property (@(posedge clk)  ((req1 == 1 & state == 0) |-> (gnt1 == 1)) iff ((req1 == 1 && state == 0) |-> (gnt1 == 1)));
+assert property (@(posedge clk)  (req1 == 0 |-> gnt1 == 0));
+assert property (@(posedge clk)  ((req1 == 0) |-> (gnt1 == 0)) iff (req1 == 0 |-> gnt1 == 0));
+assert property (@(posedge clk)  ((req1 == 1 && req2 == 0) |-> (gnt1 == 1)));
+assert property (@(posedge clk)  ((req1 == 1 & req2 == 0) |-> (gnt1 == 1)) iff ((req1 == 1 && req2 == 0) |-> (gnt1 == 1)));
+assert property (@(posedge clk)  (((req1 == 1) && (state == 0)) |-> (gnt2 == 0)));
+assert property (@(posedge clk)  ((req1 == 1 & state == 0) |-> (gnt2 == 0)) iff (((req1 == 1) && (state == 0)) |-> (gnt2 == 0)));
+assert property (@(posedge clk)  (((req2 == 1) && (state == 1)) |-> (gnt2 == 1)));
+assert property (@(posedge clk)  ((req2 == 1 & state == 1) |-> (gnt2 == 1)) iff (((req2 == 1) && (state == 1)) |-> (gnt2 == 1)));
+assert property (@(posedge clk)  ((req2 == 0) |-> (gnt2 == 0)));
+assert property (@(posedge clk)  ((req2 == 0) |-> (gnt2 == 0)) iff ((req2 == 0) |-> (gnt2 == 0)));
+assert property (@(posedge clk)  ((req2 == 1 && req1 == 0) |-> (gnt2 == 1)));
+assert property (@(posedge clk)  ((req2 == 1 & req1 == 0) |-> (gnt2 == 1)) iff ((req2 == 1 && req1 == 0) |-> (gnt2 == 1)));
+
+endmodule
---
+++ Evaluation/Dataset/i2c/i2c_Gold-ft:gpt-4o-mini-2024-07-18:nyuccs::BUh3lZyT.sv
@@ -0,0 +1,43 @@
+module i2c #(parameter divider = 125, parameter CBITS = 9) (input clk, input rst, input scl_not_ena, output reg data_clk);
+	reg [CBITS - 1:0] cnt;	//0 to 4*divider
+	reg scl_clk;
+	reg stretch;
+	always @(posedge clk) begin
+		if(rst == 1) begin
+			stretch = 0;
+			cnt = 0;
+		end
+		if(cnt >= divider*4 - 1)
+			cnt = 0;
+		else if(stretch == 0)
+			cnt = cnt + 1;
+
+		if( cnt <= divider - 1) begin
+			scl_clk = 0;
+			data_clk = 0;
+		end
+		else if( divider <= cnt && cnt <= 2*divider - 1) begin
+			scl_clk = 0;
+			data_clk = 1;
+		end
+		else if( 2*divider <= cnt && cnt <= 3*divider - 1) begin
+			if(scl_clk == 0 & scl_not_ena == 0)
+				stretch = 1;
+			else
+				stretch = 0;
+			scl_clk = 1;
+			data_clk = 1;
+		end
+		else begin
+			scl_clk = 1;
+			data_clk = 0;
+		end
+	end
+
+
+assert property(@(posedge clk) s_eventually rst == 1 || scl_not_ena == 1 || stretch == 1);
+
+assert property (@(posedge clk)  (rst or (scl_not_ena or stretch)));
+assert property (@(posedge clk)  (s_eventually rst == 1 || scl_not_ena == 1 || stretch == 1) iff (rst or (scl_not_ena or stretch)));
+
+endmodule
